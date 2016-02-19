07713338401ed5088f62446bdb0784f60d6acf78
JavierGuillamon <javier.guillamon@live.u-tad.com>
2016-02-19 15:13:14 +0100

    mal

---
+++ TallerUnity-master/Assets/Scripts/Enemy/Boss/Shoot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+    public Transform muzzle;
+    public Projectile projectile;
+    public float msBetweenShots = 100;
+    public float muzzleVelocity = 35;
+    float nextShotTime;
+    public void Shoot()
+    {
+        if (Time.time > nextShotTime)
+        {
+            nextShotTime = Time.time + msBetweenShots / 1000;
+            Projectile newProjectile = Instantiate(projectile, muzzle.position, muzzle.rotation) as Projectile;
+            newProjectile.SetSpeed(muzzleVelocity);
+        }
+    }
+}
---
+++ TallerUnity-master/Assets/Scripts/PruebaMatar.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PruebaMatar : MonoBehaviour
+{
+    public Animator anim;
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        anim.SetBool("dead", true);
+    }
+}
